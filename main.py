4bfc1a52db9ad8373b7d5ae205061d8bb1cfe598
Tfloow <88899420+Tfloow@users.noreply.github.com>
2024-06-22 12:10:05 +0200

    better image detection and cropping

--- main.py
+++ main.py
@@ -23,11 +23,41 @@ image = np.array(image)
 if len(image.shape) > 2:
     image = image[:,:,0]
     image = 1.0 - np.array(image)
+    
+# Recenter the image
+print(image)
+
+for i in range(image.shape[0]):
+    if np.max(image[i]) != 0:
+        top = i
+        break
+for i in range(image.shape[0]-1, 0, -1):
+    if np.max(image[i]) != 0:
+        bottom = i
+        break
+for i in range(image.shape[1]):
+    if np.max(image[:,i]) != 0:
+        left = i
+        break
+for i in range(image.shape[1]-1, 0, -1):
+    if np.max(image[:,i]) != 0:
+        right = i
+        break
+
+border = 5
+image = image[max(0,top-border):min(image.shape[0],bottom+border), max(0,left-border):min(image.shape[1], right+border)]
+
+print(image.shape)
+
+plt.imshow(image)
+plt.show()
+    
 if image.shape[0] != image.shape[1] :
     # Pad the image
     padding_rows = max(0, image.shape[1] - image.shape[0])
     padding_cols = max(0, image.shape[0] - image.shape[1])
-    image = np.pad(image, ((0, padding_rows), (0, padding_cols)), mode='constant')
+    image = np.pad(image, ((int(np.floor(padding_rows/2)), int(np.ceil(padding_rows/2)))
+                           , (int(np.floor(padding_cols/2)), int(np.ceil(padding_cols/2)))), mode='constant')
     print(image.shape)
 # Need to enhance the cropping with image detection algorithm
 if image.shape[0] != 28:
